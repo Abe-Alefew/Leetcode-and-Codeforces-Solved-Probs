3a23fd873ffd0fb78c049ebc785f07b4f10f285e
Abraham Alemtesfa Alefew <75578348+Abe-Alefew@users.noreply.github.com>
2026-03-01 21:42:56 +0300

    Add Almost Prime [A] from Codeforces

---
+++ 26/A - Almost Prime/solution.py
@@ -0,0 +1,1 @@
+n = int(input())  count = [0 for i in range(n+1)] for i in range(2, n+1):    if count[i] == 0:        #it is prime        for j in range(i,n+1,i):            count[j] +=1 answer = 0for i in range(n+1):    if count[i] == 2:        answer +=1 print(answer)
