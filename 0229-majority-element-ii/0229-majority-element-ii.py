d4b153f3a2d6a6d7c640809685a4a3c247799f86
Abraham Alemtesfa Alefew <75578348+Abe-Alefew@users.noreply.github.com>
2026-03-07 08:09:11 +0300

    Time: 6 ms (71.75%), Space: 23.5 MB (37.72%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,20 +1,16 @@
 class Solution:
-# @param {integer[]} nums
-# @return {integer[]}
-  def majorityElement(self, nums):
-    if not nums:
-        return []
-    count1, count2, candidate1, candidate2 = 0, 0, 0, 1
-    for n in nums:
-        if n == candidate1:
-            count1 += 1
-        elif n == candidate2:
-            count2 += 1
-        elif count1 == 0:
-            candidate1, count1 = n, 1
-        elif count2 == 0:
-            candidate2, count2 = n, 1
-        else:
-            count1, count2 = count1 - 1, count2 - 1
-    return [n for n in (candidate1, candidate2)
-                    if nums.count(n) > len(nums) // 3]
+    def majorityElement(self, nums):
+        n = len(nums)
+        nums_dict = defaultdict(int)
+        output = []
+        for i in range(n):
+            if nums[i] in nums_dict:
+                nums_dict[nums[i]] += 1
+            else:
+                nums_dict[nums[i]] = 1
+
+        for k,v in nums_dict.items():
+            if v > (n//3):
+                output.append(k)
+        
+        return output
